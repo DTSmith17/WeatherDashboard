c7261b53e132188becd703aa7b596fa4e5d20af6
DTSmith17 <153142161+DTSmith17@users.noreply.github.com>
2024-03-04 22:03:32 -0600

    Modifying fech request to grab value from text input or text content of the button, which is the city to be searched

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
-const searchButton = document.getElementsByClassName('cityInput');
+const searchButtons = document.getElementsByClassName("cityInput");
 
-async function getApi() {
-    const cityName = document.getElementsByClassName("CityInput").value;
-    
-    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=56e36f99ead2fd7da124d40234c7f905`;
-    
-    
-    fetch(requestUrl)
+async function getApi(cityName) {
+  const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=56e36f99ead2fd7da124d40234c7f905`;
+
+  fetch(requestUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data)
-    })
+      console.log(data);
+    });
 
-    // const weatherData = await fetchData(apiUrl);
-    // const data = await weatherData.json()
-    // console.log(data)
-
+  // const weatherData = await fetchData(apiUrl);
+  // const data = await weatherData.json()
+  // console.log(data)
 }
-      
-  
-  searchButton.addEventListener('click', getApi);
+
+for (const button of searchButtons) {
+  let cityName;
+  // checking to see of button has a valid City name in the ID
+  if (button.id === "searchButton") {
+    cityName = document.getElementById("searchText").value;
+  } else {
+    cityName = button.textContent;
+  }
+  button.addEventListener("click", function () {
+    getApi(cityName);
+  });
+}
