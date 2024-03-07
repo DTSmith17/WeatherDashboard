7d3536bce3fe531144f2bbbb307a964c0620a88a
DTSmith17 <153142161+DTSmith17@users.noreply.github.com>
2024-03-07 17:57:52 -0600

    fixed apiUrl and fetch function. added displayWeatherData function call to getApI function

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 const searchButtons = document.getElementsByClassName("cityInput");
 
+//getApi- Pass in the city name and make a fetch request to get the forecast for that city
 async function getApi(cityName) {
   const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=56e36f99ead2fd7da124d40234c7f905`;
 
-  fetch(requestUrl)
+  fetch(apiUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       console.log(data);
+      displayWeatherData(data);
     });
 
-  // const weatherData = await fetchData(apiUrl);
-  // const data = await weatherData.json()
-  // console.log(data)
 }
 
+//Get data from getApi function to display
+function displayWeatherData(data) {
+
+}
+
+//
 for (const button of searchButtons) {
   let cityName;
   // checking to see of button has a valid City name in the ID
